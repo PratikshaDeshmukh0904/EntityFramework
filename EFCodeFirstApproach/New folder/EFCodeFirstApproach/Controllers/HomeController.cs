56061b3a111f9a2dda4c89ed650c97c1735dd0a8
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-03-21 11:09:00 -0700

    Entity Framework Code First Approach

---
+++ EFCodeFirstApproach/New folder/EFCodeFirstApproach/Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFCodeFirstApproach.Models;
+using System.Data.Entity;
+
+namespace EFCodeFirstApproach.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        StudentContext db = new StudentContext();
+        public ActionResult Index()
+        {
+            var data = db.Students.ToList();
+            return View(data);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Students.Add(s);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    //To transfer data from one actionmethod of controller to view
+                    //ViewBag.InsertMessage = "<script>alert('Data Inserted Successfully')</script>";
+                    TempData["InsertMessage"] = "<script>alert('Data Inserted Successfully')</script>";
+                    //ModelState.Clear();
+                    return RedirectToAction("Index");  
+                    
+                    //TempData is to send data from one action method of controller to another action method of controller 
+                }
+                else
+                {
+                    TempData["InsertMessage"] = "<script>alert('Data Not Inserted Successfully')</script>";
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View();
+
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var row = db.Students.Where(model => model.Id == Id).FirstOrDefault();
+            return View(row);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student s)
+        {
+            if(ModelState.IsValid==true)
+            {
+
+            
+            db.Entry(s).State = EntityState.Modified;
+            int a= db.SaveChanges();
+                if(a>0)
+                {
+                    TempData["UpdateM"] = "<script>alert('Data Updated')</script>";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["UpdateM"] = "<script>alert('Data Not Updated')</script>";
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View();
+        }
+        //Delete
+        public ActionResult Delete(int Id)
+        {
+            var deleterow = db.Students.Where(model => model.Id == Id).FirstOrDefault();
+            return View(deleterow);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Student s)
+        {
+            if(ModelState.IsValid == true)
+            {
+                db.Entry(s).State = EntityState.Deleted;
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    TempData["Delete"] = "<script>alert('Data Deleted')</script>";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["Delete"] = "<script>alert('Not  Deleted')</script>";
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
+        public ActionResult Details(int Id)
+        {
+            var detailrow = db.Students.Where(model => model.Id == Id).FirstOrDefault();
+            return View(detailrow);
+        }
+        
+    }
+}
---
+++ EFCodeFirstApproach/New folder/EFCodeFirstApproach/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCodeFirstApproach.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
