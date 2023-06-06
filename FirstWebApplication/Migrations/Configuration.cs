06abd912c318461f7bcd7dc98f0f1711ebdf8a63
Pratiksha Deshmukh <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2023-06-06 02:41:28 -0700

    Add files via upload

---
+++ FirstWebApplication/Migrations/Configuration.cs
@@ -0,0 +1,34 @@
+﻿namespace FirstWebApplication.Migrations
+{
+    using FirstWebApplication.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FirstWebApplication.Models.MovieDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FirstWebApplication.Models.MovieDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+            context.Movies.AddOrUpdate(i => i.Title,
+new Movie
+{
+    Title = "When Harry Met Sally",
+    ReleasedDate = DateTime.Parse("1989-1-11"),
+    Genre = "Romantic Comedy",
+    Rating = "PG",
+    Price = 7.99M
+});
+
+        }
+    }
+}
